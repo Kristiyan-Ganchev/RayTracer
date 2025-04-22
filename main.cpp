2f3bf8233e6a3d495dd4aef5b5c79552ad24dc6e
Kris Ganchev <krisgg2004@gmail.com>
2025-04-22 19:05:07 +0300

    continued working on rt added: -sphere class -hittable class -list of hittable "objects" -header for general constants and utilities

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
-#include <iostream>
-#include "vec3.h"
-#include "ray.h"
-#include "color.h"
+#include "rt_const_and_util.h"
+#include "hittable.h"
+#include "hittable_list.h"
+#include "sphere.h"
 
-color ray_color(const ray& r) {
+color ray_color(const ray& r, const hittable& world) {
+    hit_record rec;
+    if (world.hit(r, 0, infinity, rec)) {
+        return 0.5 * (rec.normal + color(1, 1, 1));
+    }
+
     vec3 unit_direction = unit_vector(r.direction());
-    auto a = 0.5 * (unit_direction.y()+1.0);
+    auto a = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
 }
 int main() {
@@ -17,6 +22,13 @@ int main() {
     // Calculate the image height, and ensure that it's at least 1.
     int image_height = int(image_width / aspect_ratio);
     image_height = (image_height < 1) ? 1 : image_height;    
+
+    // World
+
+    hittable_list world;
+
+    world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+    world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
     //Camera 
     auto focal_length=1.0;
@@ -47,7 +59,7 @@ int main() {
             auto ray_direction = pixel_center - camera_center;
             ray r(camera_center, ray_direction);
 
-            color pixel_color = ray_color(r);
+            color pixel_color = ray_color(r,world);
             write_color(std::cout, pixel_color);
         }
     }
